7741f5f5508a7d68498d8b26969d7722db82ea5c
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-10-24 18:29:14 -0600

    feat: implement to funcion decorators for query

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Header, HttpCode, Param, Post, Redirect, Req } from '@nestjs/common';
+import { Body, Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req } from '@nestjs/common';
 import type { Request } from 'express';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
@@ -7,14 +8,14 @@ export class CatsController {
     @Post()
     //@HttpCode(204)
     //@Header('Cache-Control','no-store')
-    create() {
+    async create(@Body() createCatDto : CreateCatDto ) {
         return "This action adds a new cat"
     }
 
     @Get()
     //@Redirect('https://nestjs.com', 301)
-    findAll(@Req() request: Request) : string{
-        return "This action return all casts"
+    async findAll(@Query('age') age:number, @Query('breed') breed: string) {
+        return `This action returns all cast filtered by age: ${age} and breed: ${breed}`
     }
 
     @Get(':id')
