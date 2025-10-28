13c71d7ed095ccf264bc347a3e9a71014ab719b4
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-10-28 11:46:41 -0600

    feat: add controller or suppliers to spect for testing

--- src/hello/hello.service.spec.ts
+++ src/hello/hello.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HelloService } from './hello.service';
+import { HelloController } from './hello.controller';
 
 describe('HelloService', () => {
   let service: HelloService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers:[HelloController],
       providers: [HelloService],
     }).compile();
 
--- src/projects/projects.service.spec.ts
+++ src/projects/projects.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectsService } from './projects.service';
+import { ProjectsController } from './projects.controller';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      controllers:[ProjectsController],
       providers: [ProjectsService],
     }).compile();
 
